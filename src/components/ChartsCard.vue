5aef71b94600ab7358f398c6c747e546219b30ab
Tarang Srivastava <tarang.srivastava@eluv.io>
2021-07-15 16:42:12 -0700

    working example with dummy chart

--- src/components/ChartsCard.vue
+++ src/components/ChartsCard.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="charts-card">
     <h1>{{title}} chart will be here.</h1>
+    <div id="chart">
+      <SportChart />
+    </div>
   </div>
 </template>
 
 <script>
+import SportChart from './SportChart.vue'
+
 export default {
+  components: { SportChart },
   name: "ChartsCard",
   props: {
     title: String,
@@ -22,5 +28,8 @@ export default {
   border: 1px solid #DFE0EB;
   box-sizing: border-box;
   border-radius: 16px;
+}
+#chart {
+  max-width: 200px;
 }
 </style>
---
+++ src/components/SportChart.vue
@@ -0,0 +1,34 @@
+<script>
+import { defineComponent } from 'vue'
+import { Radar } from 'vue3-chart-v2'
+
+export default defineComponent({
+  name: 'SportChart',
+  extends: Radar,
+  data () {
+    return {
+      state: {
+        chartData: {
+           labels: ['Running', 'Swimming', 'Eating', 'Cycling'],
+            datasets: [
+              {
+                label: 'Human 1',
+                data: [20, 10, 4, 2]
+              },
+              {
+                label: 'Human 2',
+                data: [10, 20, 2, 4]
+              }
+            ]
+        },
+        chartOptions: {
+          responsive: true,
+        }
+      }
+    }
+  },
+  mounted () {
+    this.renderChart(this.state.chartData, this.state.chartOptions)
+  }
+})
+</script>
