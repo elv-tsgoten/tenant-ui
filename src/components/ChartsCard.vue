ea61445cdb461b4f4048cb92e1de976616b6cf62
Tarang Srivastava <tarang.srivastava@eluv.io>
2021-07-15 16:13:24 -0700

    tried implementing chartjs

--- src/components/ChartsCard.vue
+++ src/components/ChartsCard.vue
@@ -1,20 +1,35 @@
 <template>
   <div class="charts-card">
+    <canvas id="planet-chart"></canvas>
     <h1>{{title}} chart will be here.</h1>
   </div>
 </template>
 
 <script>
+import Chart from 'chart.js'
+import planetChartData from '@/scripts/planet-data.js'
+
 export default {
   name: "ChartsCard",
   props: {
     title: String,
+  },
+  data() {
+    return {
+      planetchartdata: planetChartData,
+    }
+  },
+  async mounted() {
+    const ctx = document.getElementById('planet-chart');
+    const chart = await new Chart(ctx, this.planetChartData);  
+    console.log(chart)
+    console.log(ctx)
   }
 }
 </script>
 
 <style scoped>
-.charts-card {
+/* .charts-card {
   height: 350px;
   width: 1020px;
   margin: 10px;
@@ -22,5 +37,5 @@ export default {
   border: 1px solid #DFE0EB;
   box-sizing: border-box;
   border-radius: 16px;
-}
+} */
 </style>
