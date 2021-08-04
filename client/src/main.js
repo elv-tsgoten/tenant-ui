74f228d579c9fc8b0cc316efb0dc3c123306cde3
Tarang Srivastava <tarang.srivastava@eluv.io>
2021-08-03 22:27:08 -0700

    moved files

--- client/src/main.js
+++ client/src/main.js
@@ -2,5 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// import express from 'express'
+
+// const expressApp = express();
+// expressApp.listen(5000, () => {
+//   console.log("serer has started on port 5000");
+// });
 
 createApp(App).use(store).use(router).mount('#app')
